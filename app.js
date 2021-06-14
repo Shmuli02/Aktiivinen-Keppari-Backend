9756800a583152f98e4e14c75c5f14f364f85b81
Shmuli02 <samuli.kaleva@gmail.com>
2021-06-14 12:17:40 +0000

    token lisätty

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs',blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login',loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,17 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const { nonExistingId } = require('../tests/test_helper')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username : 1, name : 1})
@@ -11,8 +22,20 @@ blogRouter.get('/', async (request, response) => {
   blogRouter.post('/', async (request, response,next) => {
     const body = request.body
 
-    const blog = new Blog(body)
-    const user = await User.findById(body.user)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title : body.title,
+      auther : body.auther,
+      url : body.url,
+      likes : body.likes,
+      user : user._id
+    })
 
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
