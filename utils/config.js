14cc9658e3b39e9936a7d42e46d736fa7fdc10e2
Shmuli02 <samuli.kaleva@gmail.com>
2021-06-16 16:10:45 +0000

    fix config

--- utils/config.js
+++ utils/config.js
@@ -6,8 +6,10 @@ const PORT = process.env.PORT
 const MONGODB_URL = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URL
   : process.env.MONGODB_URL
+const SECRET = process.env.SECRET
 
 module.exports = {
   PORT,
-  MONGODB_URL
+  MONGODB_URL,
+  SECRET
 }
