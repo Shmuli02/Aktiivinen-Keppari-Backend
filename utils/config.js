1ad9b8efee0f3650022dd94392815311906bdc99
Shmuli02 <samuli.kaleva@gmail.com>
2021-06-19 11:03:36 +0000

    config edit

--- utils/config.js
+++ utils/config.js
@@ -7,9 +7,17 @@ const MONGODB_URL = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URL
   : process.env.MONGODB_URL
 const SECRET = process.env.SECRET
+const REACT_APP_ACCESS_ID = process.env.REACT_APP_ACCESS_ID
+const REACT_APP_ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY
+const REACT_APP_BUCKET_NAME = process.env.REACT_APP_BUCKET_NAME
+const REACT_APP_REGION = process.env.REACT_APP_REGION
 
 module.exports = {
   PORT,
   MONGODB_URL,
-  SECRET
+  SECRET,
+  REACT_APP_ACCESS_ID,
+  REACT_APP_ACCESS_KEY,
+  REACT_APP_BUCKET_NAME,
+  REACT_APP_REGION
 }
