e23389d6b4fb8ff3a64eb2fee8ede40676e8a430
Shmuli02 <samuli.kaleva@gmail.com>
2021-06-30 09:27:41 +0000

    task description

--- models/task.js
+++ models/task.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose')
 
 const taskSchema = new mongoose.Schema({
   title: {type: String, require: true},
-  difficulty: {type: String, require: true}
+  difficulty: {type: String, require: true},
+  description: {type: String}
 })
 
-// taskSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//   }
-// })
+taskSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+  }
+})
 
 module.exports = mongoose.model('Task', taskSchema)
