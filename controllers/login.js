447ac01ccc3c4d58a08b0863a3e7ab856636f26c
Shmuli02 <samuli.kaleva@gmail.com>
2021-06-30 16:12:10 +0000

    token expire time changed

--- controllers/login.js
+++ controllers/login.js
@@ -26,7 +26,7 @@ loginRouter.post('/', async (request, response) => {
   const token = jwt.sign(
     userForToken, 
     process.env.SECRET,
-    {expiresIn: 60*60}
+    {expiresIn: 60*60*24*60}
   )
 
   response
